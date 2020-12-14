ffc4d4bb6a81c333a411eed6ce50382b83ac6b85
Jan Miksovsky <jan@miksovsky.com>
2020-12-13 20:50:53 -0800

    Better gloss

--- src/EmojiGloss.js
+++ src/EmojiGloss.js
@@ -11,6 +11,7 @@ import ReactiveElement from "../node_modules/elix/src/core/ReactiveElement.js";
 import emojiEntries from "./emojiEntries.js";
 
 let emojiMap;
+let longestEntryLength;
 
 export default class EmojiGloss extends ReactiveElement {
   get [defaultState]() {
@@ -52,12 +53,16 @@ export default class EmojiGloss extends ReactiveElement {
 async function getEmojiMap() {
   if (!emojiMap) {
     emojiMap = new Map();
+    longestEntryLength = 0;
     const entries = await emojiEntries;
     for (const entry of entries) {
       const { emoji, description } = entry;
       // Take first description for glosses.
       if (!emojiMap.get(emoji)) {
         emojiMap.set(emoji, description);
+      }
+      if (emoji.length > longestEntryLength) {
+        longestEntryLength = emoji.length;
       }
     }
   }
@@ -66,12 +71,29 @@ async function getEmojiMap() {
 
 async function gloss(text) {
   const map = await getEmojiMap();
-  const mapped = [...text].map((character) => {
-    const description = map.get(character);
-    return description || character;
-  });
-  const result = mapped.join(" ");
+  let result = "";
+  let remaining = text;
+  while (remaining) {
+    const { meaning, rest } = longestMatch(map, remaining);
+    result += meaning + " ";
+    remaining = rest;
+  }
   return result;
+}
+
+// Find the longest match in the map
+function longestMatch(map, text) {
+  for (let length = longestEntryLength; length > 0; length--) {
+    const candidate = text.substring(0, length);
+    const meaning = map.get(candidate);
+    if (meaning) {
+      const rest = text.slice(length);
+      return { meaning, rest };
+    }
+  }
+  // No match, skip character and return empty meaning.
+  // TODO: Use https://github.com/flmnt/graphemer
+  return { meaning: "", rest };
 }
 
 customElements.define("emoji-gloss", EmojiGloss);
