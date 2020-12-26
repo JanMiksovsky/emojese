4341e0803e55db21a61057aa2ef9733c663ebdbf
Jan Miksovsky <jan@miksovsky.com>
2020-12-26 14:52:33 -0800

    Redirect http to https.

--- app.js
+++ app.js
@@ -2,7 +2,22 @@ import express from "express";
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+app.get("*", (request, response, next) => {
+  if (
+    request.headers["x-forwarded-proto"] &&
+    request.headers["x-forwarded-proto"] !== "https"
+  ) {
+    // Redirect http to https.
+    const url = `https://${request.hostname}${request.url}`;
+    response.redirect(301, url);
+  } else {
+    next();
+  }
+});
+
 app.use(express.static("static"));
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
