c38e250ef8847549078c41f5e9f54282ed9bf63e
Jan Miksovsky <jan@miksovsky.com>
2022-12-31 11:57:22 -0800

    Update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,9 @@
-import { terser } from "rollup-plugin-terser";
-
 export default {
   input: "src/EmojeseComposer.js",
   output: [
     {
       file: "static/build.js",
       format: "es",
-      plugins: [terser()],
     },
   ],
 };
